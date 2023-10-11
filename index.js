559f659d4c86f323820e75735e429c434dc28aa1
smessie <smessie@smessie.com>
2023-10-11 10:25:05 +0200

    chore: Set autoRewrite to true

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/', createProxyMiddleware({target: 'http://prefix.cc', changeOrigin: true}));
+app.use('/', createProxyMiddleware({target: 'http://prefix.cc', changeOrigin: true, autoRewrite: true}));
 app.listen(8080);
