55a968a7dfc2d1bdb42f80bc2ce465f2139a4840
smessie <smessie@smessie.com>
2023-10-11 11:22:32 +0200

    chore: Allow explicit hostname definition via env var to fix autoRewrite with subdomains

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/', createProxyMiddleware({target: 'http://prefix.cc', changeOrigin: true, autoRewrite: true}));
+app.use('/', createProxyMiddleware({target: 'http://prefix.cc', changeOrigin: true, autoRewrite: true, hostRewrite: process.env.HOSTNAME, protocolRewrite: 'https'}));
 app.listen(8080);
