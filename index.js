d574689cb4df1528d994ffff38aaece2f421e72b
smessie <smessie@smessie.com>
2023-10-11 15:31:26 +0200

    feat: Manually perform rewrites and replace prefix.cc with HOSTNAME in response to fix CSS

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const {createProxyMiddleware, responseInterceptor} = require('http-proxy-middleware');
+const url = require('url');
 
 const app = express();
 
-app.use('/', createProxyMiddleware({target: 'http://prefix.cc', changeOrigin: true, autoRewrite: true, hostRewrite: process.env.HOSTNAME, protocolRewrite: 'https'}));
+app.use('/', createProxyMiddleware({
+    target: 'http://prefix.cc',
+    changeOrigin: true,
+    selfHandleResponse: true,
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        let response = responseBuffer.toString('utf8');
+        if (process.env.HOSTNAME) {
+            response = response.replaceAll('http://prefix.cc/', `https://${process.env.HOSTNAME}/`);
+        }
+
+        // Manually perform hostRewrite and protocolRewrite
+        if (proxyRes.headers['location']) {
+            const u = url.parse(proxyRes.headers['location']);
+            if (process.env.HOSTNAME) {
+                u.host = process.env.HOSTNAME;
+                u.protocol = 'https';
+            } else {
+                console.log(u.host);
+                console.log(req.headers['host']);
+                u.host = req.headers['host'];
+            }
+            res.setHeader('location', u.format());
+        }
+
+        return response;
+    })
+}));
 app.listen(8080);
